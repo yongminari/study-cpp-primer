0c79d660b4e695c5ad558a61cc1f1abe5bf4f652
yongminari <easyid21c@gmail.com>
2024-09-05 22:14:26 +0900

    add chapter13 sketch
    Co-authored-by: yongminari <easyid21c@gmail.com>

---
+++ Chapter13/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+  string s1 = "a value", s2 = "another";
+
+  s1 + s2 = "wow!!";
+  return 0;
+}
